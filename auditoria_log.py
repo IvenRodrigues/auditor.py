98f8d64abd65ab3e49dcd0ae08442ba575d4521a
Iven Rodrigues <ivenrodriguesww@gmail.com>
2026-03-30 21:35:21 -0300

    Melhoria na lógica de argumentos e logs

--- auditoria_log.py
+++ auditoria_log.py
@@ -1,22 +1,31 @@
 import sys
+import datetime
+import os
 
-# 1. VERIFICAÇÃO DE ARGUMENTOS (Porteiro do Script)
+# 1. VERIFICAÇÃO DE ARGUMENTOS
 if len(sys.argv) < 2:
-    print("ERRO: Você esqueceu de passar o nome do log!")
+    print("ERRO: Informe o caminho do ARQUIVO de log (ex: /var/log/sys.log)")
     sys.exit(1)
+
+pasta_alvo = sys.argv[1]
 
 try:
     palavra = "ERRO"
     total_erros = 0
 
-    # 2. ABRE O LOG PARA LEITURA E O RELATORIO PARA ANEXAÇÃO
+    # 3. ABRE O LOG PARA LEITURA E O RELATORIO PARA ANEXAÇÃO
     # sys.argv[1] é o arquivo que você passa via CLI
-    with open(sys.argv[1], "r") as log, open("erros.txt", "a") as saida:
-        for linha in log:
-            # PADRONIZA A LINHA PARA MAIÚSCULAS APENAS NA CHECAGEM
-            if palavra in linha.upper():
-                saida.write(linha)  # GRAVA A LINHA ORIGINAL
-                total_erros += 1
+    with open("erros.txt", "a") as saida:
+        for item in os.listdir(pasta_alvo):
+            caminho_completo = f"{pasta_alvo}/{item}"
+            if os.path.isfile(caminho_completo):
+                with open(caminho_completo, "r") as log:
+                    for linha in log:
+                        if palavra in linha.upper():
+                            carimbo = datetime.datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
+                            # GRAVA A LINHA ORIGINAL COM O HORÁRIO EXATO
+                            saida.write(f"{carimbo} - {linha}")
+                            total_erros += 1
 
     print(f"Linhas adicionadas ao arquivo erros.txt")
     print(f"Total de erros salvos: {total_erros}")
@@ -24,7 +33,7 @@ try:
 except OSError as e:
     # O 'f' antes das aspas permite usar {e} para mostrar o erro real do sistema
     print(f"ERRO DE INFRAESTRUTURA: {e}")
-    sys.exit(1) 
+    sys.exit(1)
 
 finally:
     print("--- Processo de auditoria encerrado! ---")
